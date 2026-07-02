3c9a2bcf84406396994c9b152b42e28d3ee573b4
klar-templates <klar.templates@twixly.io>
2026-07-02 20:53:12 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -251,12 +251,12 @@ window.setPosts = function setPosts(category, tag, topic, onError) {
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="12" y2="12"></line><line x1="4" x2="20" y1="6" y2="6"></line><line x1="4" x2="20" y1="18" y2="18"></line></svg>';
   const ICON_X =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg>';
-  // if (menuToggle && mobileMenu) {
-  //   menuToggle.addEventListener("click", () => {
-  //     const open = mobileMenu.classList.toggle("hidden") === false;
-  //     menuToggle.innerHTML = open ? ICON_X : ICON_MENU;
-  //   });
-  // }
+  if (menuToggle && mobileMenu) {
+    menuToggle.addEventListener("click", () => {
+      const open = mobileMenu.classList.toggle("hidden") === false;
+      menuToggle.innerHTML = open ? ICON_X : ICON_MENU;
+    });
+  }
 
   document.addEventListener("click", (e) => {
     const menuToggle = e.target.closest("mobile-menu-toggle");
